d06b5648d2792666b1c24e8a5a4d695857a9d47e
Chris Garrett <chris@abstraktion.co.uk>
2012-06-13 19:16:24 +0100

    Browser positioning

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,7 +1,17 @@
 /*
- * Authors:       Jamie Dyer (http://kernowsoul.com)
+ * Authors:       Jamie Dyer (http://kernowsoul.com), Chris Garrett (http://abstraktion.co.uk)
  * Last changed:  2012-06-11
  */
 
 // setup the applications namespace
 window.App = {};
+
+jQuery("document").ready(function(){
+  jQuery(window).scroll(function(){
+    if($(this).scrollTop() > 0){
+      $("body").addClass("in_scroll");
+    }else{
+      $("body").removeClass("in_scroll");
+    }
+  });
+});
