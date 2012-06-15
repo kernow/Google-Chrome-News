9b554dee1242fef2394765e00b1dd9bac07c1103
Jamie Dyer <jamie@kernowsoul.com>
2012-06-15 11:55:48 +0100

    Chore: added compiled templates to the repo

---
+++ app/javascripts/templates.js
@@ -0,0 +1,23 @@
+App.templates = {"article": function(obj){
+var __p='';var print=function(){__p+=Array.prototype.join.call(arguments, '')};
+with(obj||{}){
+__p+='';
+ if (image) { 
+;__p+='\n  <img src="'+
+( image )+
+'" />\n';
+ } 
+;__p+='\n<p class="news_category">'+
+( category )+
+'</p>\n<div class="news_meta">\n  <h1 class="primary_header"><a href="#" rel="bookmark">'+
+( title )+
+'</a></h1>\n  <p class="news_meta_item">'+
+( source )+
+'</p>\n  <p class="news_meta_item timeago" title="'+
+( new Date(updated).toISOString() )+
+'">'+
+( updated )+
+'</p>\n</div>\n';
+}
+return __p;
+}};
