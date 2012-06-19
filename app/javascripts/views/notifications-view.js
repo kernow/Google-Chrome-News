e6054895c0f071b9b69e34f316e758cdd3a81319
Jamie Dyer <jamie@kernowsoul.com>
2012-06-19 12:00:10 +0100

    Fix: only display 1 notification at a time

--- app/javascripts/views/notifications-view.js
+++ app/javascripts/views/notifications-view.js
@@ -1,35 +1,64 @@
 /*global App: false, webkitNotifications: false */
 
 App.NotificationsView = Backbone.View.extend({
+
   initialize: function(){
-    var self = this;
-    this.articlesAdded = 0;
+    var self                  = this;
+    this.unreadArticles       = 0;
+    this.previousUnreadCount  = 0;
+
     _.bindAll(this, 'add');
+
+    // Add a handler for the collections add event
     this.collection.bind('add', this.add);
 
+    // Check to see if we need to display a notification every 30 seconds
     setInterval(function() {
       self.render();
     }, 30000);
   },
 
   add: function(){
-    console.log('-- added');
-    this.articlesAdded++;
+    this.unreadArticles++;
   },
 
   render: function(){
-    if(this.articlesAdded > 0){
-      var notification = webkitNotifications.createNotification(
+    var self = this;
+
+    if(this.unreadArticles > 0 && this.previousUnreadCount != this.unreadArticles){
+
+      this.previousUnreadCount = this.unreadArticles;
+
+      // If a notification window is already being displayed then close it so only 1
+      // is ever displayed to the user
+      if(this.notification){ this.close(); }
+
+      // build the notification object
+      this.notification = webkitNotifications.createNotification(
         '48.png',
-        'You have ' + this.articlesAdded + ' new articles in the Google News App',
+        'You have ' + this.unreadArticles + ' new articles in the Google News App',
         'Click to launch the app'
       );
-      notification.onclick = function(){
+
+      // Handle notification click events
+      this.notification.onclick = function(){
+
+        // Reset the unread counts to 0
+        this.unreadArticles       = 0;
+        this.previousUnreadCount  = 0;
+
+        // Launch the application
         chrome.management.launchApp(chrome.i18n.getMessage("@@extension_id"));
-        this.close();
+        self.close();
       };
-      notification.show();
-      this.articlesAdded = 0;
+
+      // Display the notification windoe
+      this.notification.show();
     }
+  },
+
+  // close the notification window
+  close: function(){
+    this.notification.close();
   }
 });
