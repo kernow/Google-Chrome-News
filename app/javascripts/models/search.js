9162640f9373f3aa6cc7ca2693aac9312b722a9e
Jamie Dyer <jamie@kernowsoul.com>
2012-06-21 23:56:13 +0100

    Fix: support articles with no images

--- app/javascripts/models/search.js
+++ app/javascripts/models/search.js
@@ -11,10 +11,16 @@ App.SearchResults = Backbone.Collection.extend({
 
   model: App.SearchResult,
 
+  initialize: function(){
+    this.on('articleGrabbedWithImage',  this.storeImage,  this);
+    this.on('articleGrabbed',           this.saveItem,    this);
+    this.on('imageGrabbed',             this.saveItem,    this);
+  },
+
   getFromFeed: function(query, feed){
     // clear the collection of any previous results
     this.removeAll();
-    var collection = this;
+    var self = this;
     var feedUri = feed.uri({ 'query': query });
     console.warn('getting news from: ' + feedUri);
     jQuery.getFeed({
@@ -26,10 +32,12 @@ App.SearchResults = Backbone.Collection.extend({
           var parsedItem = feed.parseItem(item);
 
           // Only store the image and save the article if it not already in teh database
-          if(!collection.get(item.id)){
-            console.log('Adding and article');
-            // TODO add callback argument to storeImage call
-            collection.storeImage(parsedItem);
+          if(!self.get(item.id)){
+            if(parsedItem.image){
+              self.trigger('articleGrabbedWithImage', parsedItem);
+            }else{
+              self.trigger('articleGrabbed', parsedItem);
+            }
           }
         });
       }
@@ -43,10 +51,10 @@ App.SearchResults = Backbone.Collection.extend({
   },
 
   // grabbs the remote image linked in the article and saves it to the local store
-  storeImage: function(item, callback){
+  storeImage: function(item){
     // TODO only request images that haven't already been downloaded
     var xhr = new XMLHttpRequest();
-    var collection = this;
+    var self = this;
     xhr.responseType = "arraybuffer";
     xhr.onload = function() {
       var d = xhr.response;
@@ -57,7 +65,7 @@ App.SearchResults = Backbone.Collection.extend({
         {data: d, type: contentType},
         function(fileEntry, fileWriter) {
           item.image = fileEntry.toURL();
-          collection.saveItem(item);
+          self.trigger('imageGrabbed', item);
         },
         function(e) {console.warn(e);}
       );
